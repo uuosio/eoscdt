67fb178ddf2e3945f1d74fa1aa852db2f7d3bd9d
learnforpractice <learnforpractice@gmail.com>
2022-10-14 09:58:29 +0800

    workaround pypi file size limit

--- pysrc/__init__.py
+++ pysrc/__init__.py
@@ -8,12 +8,36 @@ import argparse
 
 __version__ = "0.1.0"
 
+def get_platform_name():
+    if platform.system() == "Linux":
+        print("manylinux1_x86_64")
+    elif platform.system() == "Windows":
+        print("win-amd64")
+    elif platform.system() == "Darwin":
+        print("macosx_10_15_x86_64")
+    else:
+        raise Exception("Unknown")
+
+def init():
+    dir_name = os.path.dirname(os.path.realpath(__file__))
+    dir_name = os.path.join(dir_name, "release")
+    if os.path.exists(dir_name):
+        return
+    print('due to the file size limit of pypi, eoscdt installed from pypi does not include binary releases. reinstalling...')
+    from pip._internal.cli.main import main as _main
+    platfrom_name = get_platform_name()
+    _main(['install', '--force-reinstall', f'https://github.com/uuosio/pycdt/releases/download/v0.1.0/eoscdt-{__version__}-py3-none-{platfrom_name}.whl'])
+
 def run_cmd(cmd: str) -> int:
+    init()
     dir_name = os.path.dirname(os.path.realpath(__file__))
     dir_name = os.path.join(dir_name, "release")
     cmd = os.path.join(dir_name, f"bin/{cmd}")
     sys.argv[0] = cmd
     return subprocess.call(sys.argv, stdout=sys.stdout, stderr=sys.stderr)
+
+def run_cdt_init():
+    return run_cmd('cdt-init')
 
 def run_cdt_cpp():
     return run_cmd('cdt-cpp')
